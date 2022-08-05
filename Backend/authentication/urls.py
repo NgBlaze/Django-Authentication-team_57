52cece6dbfc29ad1def5bb65cd7e1217bb1b8ec5
Lilywd <99019332+Lilywd@users.noreply.github.com>
2022-08-05 15:35:15 +0300

    Add files via upload

---
+++ Backend/authentication/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('signin', views.signin, name="signin"),
+    path('register', views.register, name="register"),
+    path('signout', views.signout, name="signout"),
+    path("contact", views.contact, name="contact"),
+    path("password_reset", views.password_reset_request, name="password_reset")
+]
